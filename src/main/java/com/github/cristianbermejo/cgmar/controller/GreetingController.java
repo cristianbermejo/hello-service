0bae23d4381df7ea431cb7da436e6a241068a292
Cristian Bermejo <cristian.berme@gmail.com>
2023-03-06 16:41:20 +0100

    Added method that returns JSON data

--- src/main/java/com/github/cristianbermejo/cgmar/controller/GreetingController.java
+++ src/main/java/com/github/cristianbermejo/cgmar/controller/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.cristianbermejo.cgmar.model.Info;
+
 @RestController
 public class GreetingController {
 
@@ -16,5 +18,10 @@ public class GreetingController {
 	@GetMapping(value = "greet/{n}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String greeting(@PathVariable("n") String name) {
 		return "Welcome to REST Mr./Mrs. " + name;
+	}
+	
+	@GetMapping(value = "clientinfo", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Info information() {
+		return new Info("Test", 30, "test@gmail.com");
 	}
 }
