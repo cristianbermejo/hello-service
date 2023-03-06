1d922e9b9b4e83aed7e0be90f344a018b5922c72
Cristian Bermejo <cristian.berme@gmail.com>
2023-03-06 16:30:05 +0100

    Added method with path variable

--- src/main/java/com/github/cristianbermejo/cgmar/controller/GreetingController.java
+++ src/main/java/com/github/cristianbermejo/cgmar/controller/GreetingController.java
@@ -2,6 +2,7 @@ package com.github.cristianbermejo.cgmar.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class GreetingController {
 	@GetMapping(value = "greet", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String greeting() {
 		return "Welcome to REST";
+	}
+	
+	@GetMapping(value = "greet/{n}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String greeting(@PathVariable("n") String name) {
+		return "Welcome to REST Mr./Mrs. " + name;
 	}
 }
